abc45acdbcc7236b9652d507c38f6b3161a3c9c9
Guo Qijun <240791268@qq.com>
2018-04-26 21:50:05 +0800

    0.2.1 增加条目修改功能
    点击条目，可以修改，按Enter确认修改并同步到localStorage中

--- Todo-localStroage/todo.js
+++ Todo-localStroage/todo.js
@@ -1,146 +1,96 @@
-/*
-1、在输入框输入文本之后，按add可以创建一条todo
-*/
-var todos = []
-var todo = function(text) {
-    var o = {}
-    o.text = text
-    if (todos[todos.length - 1] == undefined) {
-        o.id = 0
-    } else {
-        o.id = todos[todos.length - 1].id + 1
-    }
-    log('todo-->', o)
-    return o
-}
-var createTodo = function() {
-    var text = getInputText()
-    var t = todo(text)
-    insertTodo(t)
-    saveToList(t)
-    // saveToLocalStroage()
-}
 
-var getInputText = function() {
-    var input = e('#todo-input')
-    return input.value
-}
-var insertTodo = function(todo) {
-    var items = e('.items')
-    var html = `
-        <div class = 'todo-item' data-id = '${todo.id}'>
-            <button type = 'button' class = 'todo-item-delete'>delete</button>
-            <button type = 'button' class = 'todo-item-complete'>complete</button>
-            <span class = 'todo-item-text'>${todo.text}</span>
-        </div>
-    `
-    items.insertAdjacentHTML('beforeend', html)
-}
-var insertTodos = function(ts) {
-    // log('ts',ts)
-    for (var i = 0; i < ts.length; i++) {
-        insertTodo(ts[i])
-    }
-}
-var saveToList = function(element) {
-    todos.push(element)
-}
-/*
-2、todo可以被删除
-*/
-// var findId = function(element) {
-//     var id = 0
-//     id = element.dataset.id
-//     return id
-// }
-// var removeTodo = function(element) {
-//     var id = findId(element)
-//     var t = findTodo(id)
-//     log()
-//     t.remove()
-// }
-// var findTodo = function(id) {
-//     return e('#id')
-// }
 /*
 第一次启动时候加载页面
 */
 var showTodos = function() {
+    //var todos = ls()
     insertTodos(todos)
 }
 /*
 button  listeners
 */
-var bindAdd = function() {
-    var t_box = e('.todo-container')
-    t_box.addEventListener('click', function(event) {
-        var self = event.target
-        if (self.classList.contains('addBtn')) {
-            // log('add')
-            createTodo()
-            saveTols()
-        }
-    })
-}
-var bindRemove = function() {
-    var t_box = e('.todo-container')
-    t_box.addEventListener('click', function(event) {
-        var self = event.target
-        if (self.classList.contains('todo-item-delete')) {
-            // log('remove')
-            var item = self.closest('.todo-item')
-            var id = item.dataset.id
-            item.remove()
-            deleteIntodos(id)
-        }
-    })
-}
-var deleteIntodos = function(id) {
-    for (var i = 0; i < todos.length; i++) {
-        if (todos[i].id == id) {
-            todos.splice(i, 1)
-        }
-    }
-    saveTols()
-}
-var bindUpdate = function() {
-    var t_box = e('.todo-container')
-    t_box.addEventListener('click', function(event) {
-        var self = event.target
-        if (self.classList.contains('todo-item-complete')) {
-            // log('remove')
-            var item = self.closest('.todo-item')
-            item.classList.toggle('done')
-        }
-    })
-}
-/*
-localStorage相关
-*/
-//判断有没有缓存，没有则创建一个localStorage
-var ls = function() {
-    if (!localStorage.getItem('todoList')) {
-        localStorage.setItem('todoList', '')
-    } else {
-        data = localStorage.getItem('todoList')
-        todos = JSON.parse(data)
+//使用表驱动法，对应增删改
+var actions = {
+    add: function() {
+        createTodo()
+        saveTols()
+    },
+    remove: function(item) {
+        var id = item.dataset.id
+        item.remove()
+        deleteIntodos(id)
+    },
+    done: function(item) {
+        var id = item.dataset.id
+        item.classList.toggle('done')
+        doneIntodos(id)
+    },
+    update: function(id, str) {
+        log('update')
+        var id = item.dataset.id
+        //弹出对话框，用于修改文本  暂时不会做……
+        // create_dialog_box()
+        updateIntodos(id, str)
     }
 }
-var saveTols = function() {
-    var data = JSON.stringify(todos)
-    localStorage.setItem('todoList', data);
-}
-/*
-main程序
-*/
+
 var bindBtns = function() {
-    bindAdd()
-    bindRemove()
-    bindUpdate()
+    var t_box = e('.todo-container')
+    t_box.addEventListener('click', function(event) {
+        var self = event.target
+        item = self.closest('.todo-item')
+        if (self.classList.contains('todo-item-done')) {
+            actions.done(item)
+        } else if (self.classList.contains('addBtn')) {
+            actions.add()
+        } else if (self.classList.contains('todo-item-delete')) {
+            actions.remove(item)
+        } else if (self.classList.contains('todo-item-text')) {
+            self.contentEditable = true
+            //弹出一个对话框，用于修改内容
+            // actions.update()
+        }
+    })
 }
+var bindEdit = function() {
+    var items = e('.items')
+    items.addEventListener('keydown', function(event) {
+        item = t_find(items, '.todo-item')
+        var self = event.target
+        if (self.classList.contains('todo-item-text')) {
+            // log('keycode-->',event.code)
+            if (event.code == 'Enter') {
+                self.contentEditable = false
+                // log('text', self.innerHTML)
+                var id = item.dataset.id
+                var str = self.innerHTML
+                actions.update(id, str)
+            }
+        }
+    })
+}
+// var bindDialog = function() {
+//     var d = e('.dialog')
+//     d.addEventListener('click', function(event) {
+//         var self = event.target
+//         if (self.classList.contains('dialog_sumit')) {
+//             var di = d.t_find('.dialog_input')
+//             var di_text = di.value
+//
+//         }
+//     })
+// }
+//用于弹窗，暂时不可用
+
+var preparePage = function() {
+    insertDialog()
+}
+
 var main = function() {
+    // preparePage()  用于弹窗，暂时不可用
     ls()
     showTodos()
     bindBtns()
+    bindEdit()
 }
 main()
--- Todo-localStroage/tools.js
+++ Todo-localStroage/tools.js
@@ -8,7 +8,118 @@ var t_find = function(element, str) {
 var t_findAll = function(element, str) {
     return element.querySelectorAll(str)
 }
+//todo 的辅助函数
+var todos = []
+var todo = function(text) {
+    //var todos = ls()
+    var o = {}
+    o.text = text
+    if (todos[todos.length - 1] == undefined) {
+        o.id = 0
+    } else {
+        o.id = todos[todos.length - 1].id + 1
+    }
+    // log('todo-->', o)
+    o.done = false
+    return o
+}
+var createTodo = function() {
+    var text = getInputText()
+    var t = todo(text)
+    insertTodo(t)
+    saveToList(t)
+}
 
-// var test_getInput = function() {
-//     log('getInputText()-->', getInputText())
+var getInputText = function() {
+    var input = e('#todo-input')
+    return input.value
+}
+
+var insertTodo = function(todo) {
+    var items = e('.items')
+    var d_status = ''
+    if (todo.done == true) {
+        d_status = 'done'
+    }
+    var html = `
+        <div class = 'todo-item ${d_status}' data-id = '${todo.id}'>
+            <button type = 'button' class = 'todo-item-delete'>delete</button>
+            <button type = 'button' class = 'todo-item-done'>done</button>
+            <span class = 'todo-item-text'>${todo.text}</span>
+        </div>
+    `
+    items.insertAdjacentHTML('beforeend', html)
+}
+var insertTodos = function(ts) {
+    // log('ts',ts)
+    for (var i = 0; i < ts.length; i++) {
+        insertTodo(ts[i])
+    }
+}
+// var insertDialog = function() {
+//     var html = `
+//         <div class = 'dialog'>
+//             <input class = 'dialog_input' type = 'text' placeholder = 'update to new' />
+//             <button class = 'dialog_sumit'>sumit</button>
+//         </div>
+//     `
+//     var body = e('body')
+//     body.insertAdjacentHTML('beforeend', html)
 // }
+//用于弹窗，暂时不可用
+var deleteIntodos = function(id) {
+    //var todos = ls()
+    for (var i = 0; i < todos.length; i++) {
+        if (todos[i].id == id) {
+            todos.splice(i, 1)
+        }
+    }
+    saveTols()
+}
+var doneIntodos = function(id) {
+    //var todos = ls()
+    for (var i = 0; i < todos.length; i++) {
+        if (todos[i].id == id) {
+            todos[i].done = !todos[i].done
+            // log('todos[i].done-->', todos[i].done)
+            break
+        }
+    }
+    saveTols()
+}
+var updateIntodos = function(id, str) {
+    for (var i = 0; i < todos.length; i++) {
+        if (todos[i].id == id) {
+            todos[i].text = str
+            // log('todos[i].done-->', todos[i].done)
+            break
+        }
+    }
+    saveTols()
+}
+var saveToList = function(element) {
+    //var todos = ls()
+    todos.push(element)
+}
+
+/*
+localStorage相关
+*/
+//判断有没有缓存，没有则创建一个localStorage，有则读取并赋值给todos
+var ls = function() {
+    if (!localStorage.getItem('todoList')) {
+        localStorage.setItem('todoList', '[]')
+    } else {
+        data = localStorage.getItem('todoList')
+    }
+    todos = JSON.parse(data)
+    // var todos = JSON.parse(data)
+    // return todos
+}
+//每次todos变化的时候都需要将数据写入到localStorage中
+var saveTols = function() {
+    //var todos = ls()
+    log('save-->', todos)
+    var data = JSON.stringify(todos)
+    localStorage.setItem('todoList', data);
+}
