6aad0d995bf3fb987b93f5e836a672a9330d01a5
Guo Qijun <240791268@qq.com>
2018-04-30 23:24:38 +0800

    。

---
+++ Todo-localStroage/tools-object.js
@@ -0,0 +1,10 @@
+var log = console.log.bind(log)
+var e = function(str) {
+    return document.querySelector(str)
+}
+var t_find = function(element, str) {
+    return element.querySelector(str)
+}
+var t_findAll = function(element, str) {
+    return element.querySelectorAll(str)
+}
