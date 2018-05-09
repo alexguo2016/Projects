6573d47a4c7e66c7ac67e837469dd173333b5675
hello2016 <240791268@qq.com>
2018-05-10 00:22:50 +0800

    0.0.2

--- block breaker/control.js
+++ block breaker/control.js
@@ -1,14 +1,14 @@
-var bindLeft = function() {
+var bindMove = function() {
     var c = e('body')
+    var playerSpeed = 10
     c.addEventListener('keydown', function(event) {
         var keyCode = event.keyCode
-        // log('click', keyCode)
-        if (keyCode == 37) {
-            px -= 10
-            // drawPlayer()
-        } else if (keyCode == 39) {
-            px += 10
+        var canMoveLeft = ((px - playerSpeed) >= 0)
+        var canMoveRight = ((px + 70 + playerSpeed) < canvasWidth)
+        if (keyCode == 37 && canMoveLeft) {
+            px -= playerSpeed
+        } else if (keyCode == 39 && canMoveRight) {
+            px += playerSpeed
         }
-        // drawPlayer()
     })
 }
--- block breaker/game.js
+++ block breaker/game.js
@@ -2,77 +2,76 @@
 var x = 200
 var dx = 1
 var dy = 1
+var canvasWidth = 600
+var canvasHeight = 400
+var ballWidth = 50
+var playerWidth = 70
+var playerHeight = 20
 var y = 200
-var px = (400/2 - 35)
-var py = 300 - 20 - 20
-var img = new Image()
-img.src = "ball.png"
+var px = (canvasWidth - playerWidth) / 2
+var py = canvasHeight - playerHeight - 20
+
+var imgs = {
+    'player': createImg('player.png'),
+    'ball': createImg('ball.png'),
+}
+
 var drawBall = function() {
     var c = e('#myCanvas')
     var cxt = c.getContext("2d")
-    cxt.clearRect(0, 0, 400, 300)
-    // cxt.clearRect(x - 2, y - 2, 54, 54)
-    cxt.drawImage(img, x, y)
+    cxt.clearRect(0, 0, canvasWidth, canvasHeight)
+    cxt.drawImage(imgs.ball, x, y)
 
     var speed = 2
-
-    var isOutX = ((x + 50) >= 400 || (x) <= 0)
-    var isOutY = ((y + 50) >= 300 || y <= 0)
-
-    var touchPlayerY = (y + 50 >= py)
-    var in_x_Detected = (x > px && x < px + 70) || (x + 50 > px && x + 50 < px + 70)
-    var touchBottom = (y + 50 >= 300)
-    var in_y_Detected = (y > py && y < py + 20) || (y + 50 > py && y + 50 < py + 20)
-    // var touchPlayerX = !((x > px + 70) || (x + 50 < px))
-    var touchPlayerX = !(x - 50 > px + 70)
-
-    if (in_x_Detected) {
-        // log('in')
-        if (touchPlayerY) {
-            // log('mayTouchY')
-            dy *= -1
-        }
-    }
-    if (in_y_Detected) {
-        // log('in')
-        if (touchPlayerX) {
-            dx *= -1
-        }
-    }
-    if (isOutX) {
-        // dx = Math.random()
-        dx *= -1
-    }
-    x += speed * dx
-    if (touchBottom) {
-        log('bottom')
-    }
-    if (isOutY) {
-        // dy = Math.random()
+    if (touchPlayer()) {
         dy *= -1
     }
+    boundaryDetect()
+    x += speed * dx
     y -= speed * dy
 }
 
-// setInterval(drawBall, 1000/50)
+var touchPlayer = function() {
+    var touchPlayerY = (y + ballWidth >= py)
+    var left_limit = (x > px && x < px + playerWidth)
+    var right_limit = (x + ballWidth > px && x + ballWidth < px + playerWidth)
+    var touchPlayerX = !(x - ballWidth > px + playerWidth)
 
-var player = new Image()
-player.src = "player.png"
-// var px = 0
-// var py = 0
+    if (left_limit || right_limit) {
+        if (touchPlayerY) {
+            return true
+        }
+    }
+    return false
+}
+
+var boundaryDetect = function() {
+    var isOutX = ((x + ballWidth) >= canvasWidth || x <= 0)
+    var isOutY = ((y + ballWidth) >= canvasHeight || y <= 0)
+    var touchBottom = (y + ballWidth >= canvasHeight)
+    if (isOutX) {
+        dx *= -1
+    }
+    if (isOutY) {
+        dy *= -1
+    }
+    if (touchBottom) {
+        log('bottom')
+    }
+}
+
 var drawPlayer = function() {
     var c = e('#myCanvas')
     var cxt = c.getContext("2d")
-    cxt.clearRect(px, py, 70, 20)
-    cxt.drawImage(player, px, py)
+    cxt.clearRect(px, py, playerWidth, playerHeight)
+    cxt.drawImage(imgs.player, px, py)
 }
 
 var __main = function() {
     setInterval(function() {
         drawBall()
         drawPlayer()
-    }, 1000/70)
-    drawPlayer()
-    bindLeft()
+    }, 1000/66)
+    bindMove()
 }
 __main()
