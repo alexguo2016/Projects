959cc32982198c1d1ac942844344a1992011a078
hello2016 <240791268@qq.com>
2018-06-21 21:43:43 +0800

    增加目录结构, 准备写页面.

---
+++ interviewList/app.js
@@ -0,0 +1,26 @@
+var express = require('express')
+var bodyParser = require('body-parser')
+var route = require('./util/routeUtil')
+
+var app = express()
+app.use(express.static('static'))
+app.use(bodyParser.json())
+
+var routesList = [
+    'index',
+    'answer',
+    'question',
+]
+
+for (var i = 0; i < routesList.length; i++) {
+    var r = routesList[i]
+    var rs = require(`./route/${r}`).routes
+    route.registerRoutes(rs, app)
+}
+
+var server = app.listen(7000, () => {
+    var instance = server.address()
+    var address = instance.address
+    var port = instance.port
+    console.log(`本地服务器启动: http://${address}:${port}`)
+})
---
+++ interviewList/task.md
@@ -0,0 +1,12 @@
+2018-6-21
+主要任务:
+1. 页面显示, 使用bootstrap和vue.js
+2. 后台增删改查
+
+主要问题:
+1. 并没有用过vue...
+2. bootstrap并不熟练...
+
+计划
+1. 先用原生JavaScript写, 后面改成vue.js
+2. bootstrap边查边用...
